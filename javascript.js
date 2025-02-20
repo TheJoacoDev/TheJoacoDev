23fd58445dc10e7ff7051cb507f8ea0010f5647c
TheJoacoDev <joacoferr@hotmail.com.ar>
2025-02-19 23:08:26 -0300

    borre la carpeta que no quise agregar, no sé por qué entró

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 var myArray = [];
 
+
 var i = 0;
 
+/*
 while (i < 5) {
     myArray.push(i);
     i++;
 }
 
+console.log(myArray);
+
+*/
+
+for (var i = 0; i < 5; i++) {
+    ourArray.push(i);
+}
 console.log(myArray);
